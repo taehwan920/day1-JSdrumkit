f001c3b740583277a0b1491590310d1440d9a7a1
Yang Taehwan <taehwan920@gmail.com>
2020-03-14 01:16:45 +0900

    made function more iterable

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-window.addEventListener('keydown', function (e) {
+function playDrum(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
     if (!audio) return;
     audio.currentTime = 0;
     audio.play();
     key.classList.add('playing');
-});
+}
+
+window.addEventListener('keydown', playDrum);
 
 function removeTransition(e) {
     if (e.propertyName !== 'transform') return;
