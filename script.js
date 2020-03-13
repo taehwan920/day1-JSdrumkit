46f2bbac892f1e811a367750222eb92c7013ff1a
Yang Taehwan <taehwan920@gmail.com>
2020-03-14 01:10:14 +0900

    the function is ready. And added boom sound

--- script.js
+++ script.js
@@ -4,4 +4,13 @@ window.addEventListener('keydown', function (e) {
     if (!audio) return;
     audio.currentTime = 0;
     audio.play();
+    key.classList.add('playing');
 });
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
