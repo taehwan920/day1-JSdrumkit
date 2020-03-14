e3cf272732b5e3cf9092b45db5f12f707c14e949
Yang Taehwan <taehwan920@gmail.com>
2020-03-14 20:59:19 +0900

    For now, we could play the drum kit by click! Wow!

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-function playDrum(e) {
+window.addEventListener('keydown', function (e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
     if (!audio) return;
     audio.currentTime = 0;
     audio.play();
     key.classList.add('playing');
-}
+});
 
-window.addEventListener('keydown', playDrum);
+window.addEventListener('click', function (E) {
+    const clickTarget = E.target.innerHTML;
+    const clickKeycode = clickTarget.charCodeAt(0);
+    const clickAudio = document.querySelector(`audio[data-key='${clickKeycode}']`);
+    if (!clickAudio) return;
+    clickAudio.currentTime = 0;
+    clickAudio.play();
+    key.classList.add('playing');
+
+})
 
 function removeTransition(e) {
     if (e.propertyName !== 'transform') return;
